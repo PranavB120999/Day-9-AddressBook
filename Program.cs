9de6b935991128cd1d1b9a690adf95746a3dd1b7
Pranav <pranavbhople99@gmail.com>
2022-03-21 20:00:32 +0530

    Main file for UC-2-EqualMethod is added

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LineComparisionProblem
+{
+    internal class Program
+    {
+        public static void UC_1_Length(int x1, int y1, int x2, int y2)
+        {
+            double result, sum;
+            sum = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+            result = Math.Sqrt(sum);
+            Console.WriteLine(result);
+        }
+
+
+        public static void UC_2_CheckLines(int a1, int b1, int a2, int b2)
+        {
+            int len1, len2;
+            if (a1 == a2)
+            {
+                Console.WriteLine(b1.Equals(b2));
+            }
+            else
+            {
+                len1 = b1 - a1;
+                len2 = b2 - a2;
+                Console.WriteLine(len1.Equals(len2));
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Line Comparison problem:");
+
+            Console.WriteLine("Please enter X co-ordinates: (x1 and y1)");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter Y co-ordinates: (x2 and y2)");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Program.UC_1_Length(x1, y1, x2, y2);
+
+            Console.WriteLine("To Check Both Line Equals or not by Equals method");
+            Console.WriteLine("Enter start and end points of First line: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter start and end points of Second line: ");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            Program.UC_2_CheckLines(a1, b1, a2, b2);
+
+
+        }
+    }
+}
